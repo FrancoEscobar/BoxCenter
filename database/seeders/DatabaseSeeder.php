60eb1bb6b647141027bc84900f40b933a309b309
FrancoEscobar <francoescobar2023@gmail.com>
2025-11-11 17:45:50 -0300

    feat(seeder): agregar MembresiaSeeder para inicializar datos de membresías con valores predeterminados

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
             TipoEntrenamientoSeeder::class,
             PlanSeeder::class,
             MetodosPagoSeeder::class,
+            MembresiaSeeder::class,
         ]);
     }
 }
